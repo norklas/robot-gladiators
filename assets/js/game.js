18ab5f592aab0c2418b83107623b8baea3a2b5fd
Nick Melanson <nickm890@gmail.com>
2022-02-03 16:50:08 -0500

    fixed fight skip bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,47 @@
 // console.log(enemyInfo.names[0]);
 // console.log(enemyInfo.names[3]);
 
+var fightOrSkip = function () {
+  // ask player if they'd like to fight or skip using fightOrSkip function
+  var promptFight = window.prompt(
+    'Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.'
+  );
+
+  // Conditional Recursive Function Call
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+  }
+
+  // if player picks "skip" confirm and then stop the loop
+  promptFight = promptFight.toLowerCase();
+
+  if (promptFight === "skip") {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+  }
+
+  // if yes (true), leave fight
+  if (confirmSkip) {
+    window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+    // subtract money from playerMoney for skipping
+    playerInfo.playerMoney = Math.max(0, playerInfo.money - 10);
+    // return true if player wants to leave
+    return true;
+  }
+  return false;
+};
+
 // fight function (now with parameter for enemy's name)
 var fight = function (enemyInfo) {
+  // repeat and execute as long as the enemy-robot is alive
   while (playerInfo.health > 0 && enemyInfo.health > 0) {
-    // ask player if they'd like to fight or run
-    var promptFight = window.prompt(
-      'Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.'
-    );
+    // ask player if they'd like to fight or skip using fightOrSkip function
+    if (fightOrSkip()) {
+      // if true, leave fight by breaking the loop
+      break;
+    }
+    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
     // if player picks "skip" confirm and then stop the loop
     if (promptFight === "skip" || promptFight === "SKIP") {
